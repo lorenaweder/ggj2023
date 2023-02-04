79be9434403df7e9c14ea7e76c713a5c27555464
Lorena <latamakuchi@gmail.com>
2023-02-04 20:08:17 +0100

    UI stuff

--- Assets/Scripts/UI/GameEnd.cs
+++ Assets/Scripts/UI/GameEnd.cs
@@ -3,17 +3,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameEnd : MonoBehaviour
 {
-    [SerializeField] private Image image;
-    [SerializeField] private Button next;
+    [SerializeField] private int _nextScene;
+    [SerializeField] private Image _image;
+    [SerializeField] private Button _next;
 
     private void Awake()
     {
-        next.interactable = false;
-        image.DOFade(0f, 0);
+        var t = transform as RectTransform;
+
+        t.anchoredPosition = Vector3.zero;
+
+        _next.interactable = false;
+        _image.DOFade(0f, 0);
     }
 
     void Start()
@@ -25,18 +31,26 @@ public class GameEnd : MonoBehaviour
     private void OnDestroy()
     {
         MessageDispatcher.OnGameOver -= ShowResult;
+        _next.onClick.RemoveAllListeners();
+    }
+
+    public void LoadRetry()
+    {
+        _next.onClick.RemoveAllListeners();
+        SceneManager.LoadScene(_nextScene);
     }
 
     private void ShowResult()
     {
+        _next.onClick.AddListener(LoadRetry);
         gameObject.SetActive(true);
         DOTween.Sequence()
-            .Append(image.DOFade(1f, 0.25f))
+            .Append(_image.DOFade(1f, 0.25f))
             .AppendCallback(EnableButtons);
     }
 
     private void EnableButtons()
     {
-        next.interactable = true;
+        _next.interactable = true;
     }
 }
